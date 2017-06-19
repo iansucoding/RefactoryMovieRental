14b3bc63bc652b95dbe4ff540373021bfa711e52
iansu <iansucoding@hotmail.com>
2017-06-19 12:07:50 +0800

    1.起始一個簡單案例:影片出租

---
+++ MovieRental.Code/Customer.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+
+namespace MovieRental.Code
+{
+    /// <summary>
+    /// 顧客
+    /// </summary>
+    public class Customer
+    {
+        private string _name;
+        private List<Rental> _rentals = new List<Rental>();
+
+        public Customer(string name)
+        {
+            this._name = name;
+        }
+
+        public void AddRental(Rental arg)
+        {
+            this._rentals.Add(arg);
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        /// <summary>
+        /// 生成樣單 (根據租貸時間和影片類型計算出費用)
+        /// </summary>
+        /// <returns></returns>
+        public string Statement()
+        {
+            double totalAmount = 0;
+            int frequentRenterPoints = 0; // 積分
+            string result = $"Rental Record for {GetName()} \n";
+            foreach (var each in _rentals)
+            {
+                double thisAmount = 0;
+                // detemine amounts for each line
+                switch (each.GetMovie().GetPriceCode())
+                {
+                    case PriceCode.Regular:
+                        thisAmount += 2;
+                        if (each.GetDaysRented() > 2)
+                        {
+                            thisAmount += (each.GetDaysRented() - 2) * 1.5;
+                        }
+                        break;
+
+                    case PriceCode.NewRelease:
+                        thisAmount += each.GetDaysRented() * 3;
+                        break;
+
+                    case PriceCode.Childrens:
+                        thisAmount += 1.5;
+                        if (each.GetDaysRented() > 3)
+                        {
+                            thisAmount += (each.GetDaysRented() - 3) * 1.5;
+                        }
+                        break;
+                }
+
+                // add frequent renter points
+                frequentRenterPoints++;
+
+                // add bonus for two day new release rental
+                if (each.GetMovie().GetPriceCode() == PriceCode.NewRelease && each.GetDaysRented() > 1)
+                {
+                    frequentRenterPoints++;
+                }
+
+                // show figures for this rental
+                result += "\t" + each.GetMovie().GetTitle() + "\t" + thisAmount.ToString() + "\n";
+                totalAmount += thisAmount;
+            }
+            // ad footer lines
+            result += "Amount owed is " + totalAmount.ToString() + "\n";
+            result += "You earned " + frequentRenterPoints.ToString() + " frequent renter points";
+            return result;
+        }
+    }
+}
---
+++ MovieRental.Code/Movie.cs
@@ -0,0 +1,53 @@
+﻿namespace MovieRental.Code
+{
+    /// <summary>
+    /// 影片 (只是一個簡單的純數據類)
+    /// </summary>
+    public class Movie
+    {
+        private string _title;
+        private PriceCode _priceCode;
+
+        public Movie(string title, PriceCode priceCode)
+        {
+            this._title = title;
+            this._priceCode = priceCode;
+        }
+
+        public PriceCode GetPriceCode()
+        {
+            return this._priceCode;
+        }
+
+        public void SetPriceCode(PriceCode arg)
+        {
+            this._priceCode = arg;
+        }
+
+        public string GetTitle()
+        {
+            return _title;
+        }
+    }
+
+    /// <summary>
+    /// 影片類型
+    /// </summary>
+    public enum PriceCode
+    {
+        /// <summary>
+        /// 普通片
+        /// </summary>
+        Regular,
+
+        /// <summary>
+        /// 新片
+        /// </summary>
+        NewRelease,
+
+        /// <summary>
+        /// 兒童片
+        /// </summary>
+        Childrens
+    }
+}
